a71fdf58ac306f0a53cd1e328b9a191534e2362f
Dan <baitursonov@gmail.com>
2023-01-11 16:13:53 +0600

    Programm added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длинна которых меньше или равна 3 символа
+string[] ss={"Hello", "sho", "sf", "sdaad", "123", "1234","e"};
+void Stringlength(string[]a)
+{
+ for (int i=0;i < ss.Length; i++)
+    if (ss[i].Length <= 3)
+    System.Console.Write($"{ss[i]}  ");
+}    
+Stringlength(ss);
